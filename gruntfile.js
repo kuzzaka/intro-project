2de2b36d78d2810eb36f518d03e672875c6b4b12
Unknown <enflaer@gmail.com>
2017-11-22 09:34:56 +0500

    IP - 2
    Поправил косяки

--- gruntfile.js
+++ gruntfile.js
@@ -6,27 +6,26 @@ module.exports = function(grunt) {
             js: {
                 src: ['src/js/*.js'],
                 dest: 'js/scripts.js',
-            }
+            },
         },
         watch: {
             js: {
-                files: ['src/js/*.js']
+                files: ['src/js/*.js'],
             },
             scss: {
-                files: ['src/css/*.scss']
+                files: ['src/css/*.scss'],
             },
         },
         sass: {
             dist: {
                 files: {
-                    'css/styles.css': ['src/css/*.scss']
-                }
-            }
+                    'css/styles.css': ['src/css/*.scss'],
+                },
+            },
         },
     });
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.registerTask('default', ['concat', 'sass', 'watch']);
-
 };
