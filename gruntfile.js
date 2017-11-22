d1b5b9f1b043b7fb13fd239ff31352f773459dff
Unknown <enflaer@gmail.com>
2017-11-22 14:43:46 +0500

    IP - 3
    1. Добавлены jq, backbone, lodash в scripts.js
    2. Сверстана форма добавления товара по артикулу

--- gruntfile.js
+++ gruntfile.js
@@ -1,31 +1,34 @@
 module.exports = function(grunt) {
-    'use strict';
+  'use strict';
 
-    grunt.initConfig({
-        concat: {
-            js: {
-                src: ['src/js/*.js'],
-                dest: 'build/js/scripts.js',
-            },
+  grunt.initConfig({
+    concat: {
+      js: {
+        src: ['node_modules/lodash/lodash.js',
+              'node_modules/jquery/dist/jquery.js',
+              'node_modules/backbone/backbone.js',
+              'src/js/*.js'],
+        dest: 'build/js/scripts.js',
+      },
+    },
+    watch: {
+      js: {
+        files: ['src/js/*.js'],
+      },
+      scss: {
+        files: ['src/css/*.scss'],
+      },
+    },
+    sass: {
+      dist: {
+        files: {
+          'build/css/styles.css': ['src/css/*.scss'],
         },
-        watch: {
-            js: {
-                files: ['src/js/*.js'],
-            },
-            scss: {
-                files: ['src/css/*.scss'],
-            },
-        },
-        sass: {
-            dist: {
-                files: {
-                    'build/css/styles.css': ['src/css/*.scss'],
-                },
-            },
-        },
-    });
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.registerTask('default', ['concat', 'sass', 'watch']);
+      },
+    },
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default', ['concat', 'sass', 'watch']);
 };
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,21 @@
-console.log('hello world!');
+$('.add-item').click(function(e) {
+  const popup = $('.window');
+
+  if (popup.hasClass('hidden')) {
+    popup.removeClass('hidden');
+
+    let firstClick = true;
+    $(document).bind('click.myEvent', function(e) {
+      if (!firstClick && $(e.target).closest('.window').length === 0) {
+        popup.addClass('hidden');
+        $(document).unbind('click.myEvent');
+      }
+      firstClick = false;
+    });
+  }
+  e.preventDefault();
+});
+$('.modal__close').click(function(e) {
+  $('.window').addClass('hidden');
+  $(document).unbind('click.myEvent');
+});
