3b264df4c42ec12de8ad1e00776cfdb81e8d9ff8
Unknown <enflaer@gmail.com>
2017-11-29 09:47:52 +0500

    Вывод характеристик и изображения товара

--- src/js/components/product-view.js
+++ src/js/components/product-view.js
@@ -8,6 +8,8 @@ var ProductView = Backbone.View.extend({
     this.$el.html(this.template({
       model: this.model.toJSON(),
     }));
+    (this.model.get('certificate_type_id') === 0) ? this.model.set('certificate_type_id', 'Не подлежит сертификации') : this.model.set('certificate_type_id', 'Сертифицирован');
+    (this.model.get('keep_package') === 0) ? this.model.set('keep_package', 'Без коробки') : this.model.set('keep_package', 'С коробкой');
     return this;
   },
 });
